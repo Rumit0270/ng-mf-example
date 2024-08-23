4a7f2c27c79b3a0a6f8e1d974f62cf6e9c6eafb5
Rumit Tandukar <rumit0270@gmail.com>
2024-08-23 21:17:39 +0545

    feat: Add deploy script

--- apps/mfe1/src/environments/environment.prod.ts
+++ apps/mfe1/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiURL: 'https://jsonplaceholder.typicode.com',
-  appURL: '',
+  appURL: 'https://d3bul7tg4b6ttv.cloudfront.net/mfe1',
   name: 'prod',
 };
